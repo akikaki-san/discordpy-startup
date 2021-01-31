7b3ce641aea630b90a28ff1da0ea996901a58e87
akikaki-san <69340721+akikaki-san@users.noreply.github.com>
2021-01-31 15:35:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,10 +11,7 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
-@bot.event
-async def on_ready():
-await bot.change_presence(activity=discord.Game(f"a!help")
+   
 
 @bot.command()
 async def ping(ctx):
