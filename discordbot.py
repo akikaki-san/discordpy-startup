ccaa2f61bb496c9af0649b86bf86a94768132181
akikaki-san <69340721+akikaki-san@users.noreply.github.com>
2021-02-01 22:07:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
 bot = commands.Bot(command_prefix=prefix, help_command=commands.MinimalHelpCommand())
 
 
